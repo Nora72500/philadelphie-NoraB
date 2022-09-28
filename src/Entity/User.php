e55f0a3c7d024fc7af5297f6603b475ca90aa834
Nora72500 <104192019+Nora72500@users.noreply.github.com>
2022-09-28 10:27:37 +0200

    security

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,84 +2,90 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
+#[UniqueEntity('email')]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
 
-    #[ORM\Column(length: 255)]
-    private ?string $user = null;
+    #[ORM\Column(type: 'string', length: 50)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 2, max: 50)]
+    private string $fullName;
 
-    #[ORM\Column(length: 255)]
-    private ?string $nom = null;
+    #[ORM\Column(type: 'string', length: 50, nullable: true)]
+    #[Assert\Length(min: 2, max: 50)]
+    private ?string $pseudo = null;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    #[Assert\Email()]
+    #[Assert\Length(min: 2, max: 180)]
+    private string $email;
+
+    #[ORM\Column(type: 'json')]
+    #[Assert\NotNull()]
+    private array $roles = [];
+
+    private ?string $plainPassword = null;
+
+    #[ORM\Column(type: 'string')]
+    #[Assert\NotBlank()]
+    private string $password = 'password';
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    #[Assert\NotNull()]
+    private \DateTimeImmutable $createdAt;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    #[Assert\NotNull()]
+    private \DateTimeImmutable $updatedAt;
 
     #[ORM\Column(length: 255)]
     private ?string $prenom = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $email = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
-
-    #[ORM\Column]
-    private ?\DateTimeImmutable $createdAt = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $fullName = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $speudo = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $pseudo = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $plainPassword = null;
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->updatedAt = new \DateTimeImmutable();
+ 
+    }
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getUser(): ?string
+    public function getFullName(): ?string
     {
-        return $this->user;
+        return $this->fullName;
     }
 
-    public function setUser(string $user): self
+    public function setFullName(string $fullName): self
     {
-        $this->user = $user;
+        $this->fullName = $fullName;
 
         return $this;
     }
 
-    public function getNom(): ?string
+    public function getPseudo(): ?string
     {
-        return $this->nom;
+        return $this->pseudo;
     }
 
-    public function setNom(string $nom): self
+    public function setPseudo(?string $pseudo): self
     {
-        $this->nom = $nom;
-
-        return $this;
-    }
-
-    public function getPrenom(): ?string
-    {
-        return $this->prenom;
-    }
-
-    public function setPrenom(string $prenom): self
-    {
-        $this->prenom = $prenom;
+        $this->pseudo = $pseudo;
 
         return $this;
     }
@@ -96,7 +102,58 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of plainPassword
+     */
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * Set the value of plainPassword
+     *
+     * @return  self
+     */
+    public function setPlainPassword($plainPassword)
+    {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -106,6 +163,15 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getCreatedAt(): ?\DateTimeImmutable
@@ -120,41 +186,28 @@ class User
         return $this;
     }
 
-    public function getFullName(): ?string
+    public function getUpdatedAt(): ?\DateTimeImmutable
     {
-        return $this->fullName;
+        return $this->updatedAt;
     }
 
-    public function setFullName(string $fullName): self
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
     {
-        $this->fullName = $fullName;
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
 
-   
-
-    public function getPseudo(): ?string
+    public function getPrenom(): ?string
     {
-        return $this->pseudo;
+        return $this->prenom;
     }
 
-    public function setPseudo(string $pseudo): self
+    public function setPrenom(string $prenom): self
     {
-        $this->pseudo = $pseudo;
+        $this->prenom = $prenom;
 
         return $this;
     }
 
-    public function getPlainPassword(): ?string
-    {
-        return $this->plainPassword;
-    }
-
-    public function setPlainPassword(string $plainPassword): self
-    {
-        $this->plainPassword = $plainPassword;
-
-        return $this;
-    }
 }
